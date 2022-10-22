fe5cd0e3bcf40869dbf5eae634d86627fa6a41fd
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-10-23 01:58:03 +0300

    s

--- Assets/Scripts/Enemy/EnemyBullet/EnemyBulletMovement.cs
+++ Assets/Scripts/Enemy/EnemyBullet/EnemyBulletMovement.cs
@@ -22,6 +22,6 @@ public class EnemyBulletMovement : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D other)
     {
-        Destroy(this.gameObject, lifeTime);
+        Destroy(this.gameObject);
     }
 }
