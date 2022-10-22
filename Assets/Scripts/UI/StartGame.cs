92a3f93ae03b33f21b90e4e8e20021ab443ac350
Aydan Günaydın <aydangunaydin@hotmail.com>
2022-10-22 14:29:59 +0300

    start scene

---
+++ Assets/Scripts/UI/StartGame.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartGame : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
