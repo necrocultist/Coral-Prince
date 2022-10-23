530726c2d61e479cafb476089a62ca6a270f6a97
karataskn20 <karataskn20@gmail.com>
2022-10-23 12:54:51 +0300

    update enemy ac

---
+++ Assets/Scripts/Audio/SoundManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager Instance;
+    [SerializeField] private AudioSource musicSource;
+    [SerializeField] private AudioSource effectSource;
+
+
+    private void OnEnable()
+    {
+        //SceneManager.activeSceneChanged += ChangeActiveMusic;
+    }
+
+    private void OnDisable()
+    {
+        //SceneManager.activeSceneChanged -= ChangeActiveMusic;
+    }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void PlaySound(AudioClip clip)
+    {
+        effectSource.PlayOneShot(clip);
+    }
+
+    public void ChangeMasterVolume(float value)
+    {
+        AudioListener.volume = value;
+    }
+
+    public void ToggleEffects()
+    {
+        effectSource.mute = !effectSource.mute;
+    }
+
+    public void ToggleMusic()
+    {
+        musicSource.mute = !musicSource.mute;
+    }
+
+    //private void ChangeActiveMusic(Scene current, Scene next)
+    //{
+    //    if (SceneManager.GetActiveScene().buildIndex == 0)
+    //    {
+    //        musicSources[0].mute = false;
+    //        musicSources[1].mute = true;
+    //    }
+    //    else if (SceneManager.GetActiveScene().buildIndex == 1)
+    //    {
+    //        musicSources[0].mute = true;
+    //        musicSources[1].mute = false;
+    //    }
+    //}
+}
